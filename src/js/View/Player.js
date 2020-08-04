b434069f1a1b9249d066ea8218cc1fba5533a5d4
REG0129 <k1221k0129k0210@gmail.com>
2020-08-05 01:33:37 +0900

    質問の為プッシュ

--- src/js/View/Player.js
+++ src/js/View/Player.js
@@ -6,21 +6,24 @@ import HitTest from "js/Util/HitTest";
  */
 export default class Player extends UnitBase {
     constructor () {
-        super ();
+        super (); //親constructor → MainManager.js
         this.x = 100;
         this.y = 200;
         this.setHP(100);
         this.setWidth(40);
         this.setHeight(40);
     }
-    
+
     /**
      * EnterFrame.jsの中で
      * requestAnimationFrameから自動的にcallされ続けます。
      */
     update () {
         // 矢印キー　←↑→↓で動くようにしてください。googleで「js keycode」など検索してみて下さい。
-
+        this.x = this.x + playerHorizontal;
+        this.y = this.y + playerVertical;
+        playerHorizontal = 0; //1fずつリセット
+        playerVertical = 0; //1fずつリセット
         // スペースキーを押すとBulletが発射されるようにして下さい。
         // Enemyクラスを参考にしてください。
 
@@ -40,11 +43,11 @@ export default class Player extends UnitBase {
     /**
      * 機体描画
      * 三角形
-     * @param {ctx} context 
+     * @param {ctx} context
      */
     draw (context) {
 		context.beginPath();
-		context.moveTo(this.x - 20, this.y + 10); 
+		context.moveTo(this.x - 20, this.y + 10);
 		context.lineTo(this.x + 20, this.y);
 		context.lineTo(this.x - 20, this.y - 10);
 		context.closePath();
@@ -56,3 +59,21 @@ export default class Player extends UnitBase {
 		context.fill();
     }
 }
+
+let playerHorizontal = 0; //1f内での総量
+let playerVertical = 0; //1f内での総量
+
+addEventListener('keydown',(e) => {
+  if(e.keyCode == 39){ //right
+    playerHorizontal += 5;
+  }
+  if(e.keyCode == 37){ //left
+    playerHorizontal += -5;
+  }
+  if(e.keyCode == 40){ //up
+    playerVertical += 5;
+  }
+  if(e.keyCode == 38){ //down
+    playerVertical += -5;
+  }
+});
